3b46ef4fd9fcde8c1f2d170ab327c4ec7b1d5e20
Eric Lozano <ericlozano98@gmail.com>
2020-05-31 21:04:09 +0200

    New version of Categories

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,38 +1,29 @@
 
 function switch_visibility(id) {
 
-	var post_class_name = id.concat("-post");
+	var posts_id = id.concat("-posts");
 
-	var posts = document.getElementsByClassName(post_class_name);
+	var posts = document.getElementById(posts_id);
 
-	for (var i = 0; i < posts.length; i++) {
-
-		var post = posts[i];
-
-		if (post.style.display === "none") {
-			post.style.display = "block";
-		} else {
-			post.style.display = "none";
-		}
+	if (posts.style.display === "none") {
+		posts.style.display = "block";
+	} else {
+		posts.style.display = "none";
 	}
-
 
 	var button_id = id.concat("-button");
 
 	var button = document.getElementById(button_id);
 
-	console.log(button.style.backgroundColor);
-
-	if (button.style.backgroundColor == 'rgb(133, 133, 133)') {
-		button.style.backgroundColor = "#FF4CAB";
+	if (button.style.backgroundColor == 'rgb(38, 38, 38)') {
+		button.style.backgroundColor = "#FF3591";
 		button.style.color = "black";
-		console.log("Supuestamente cambiado a rosa");
+		button.style.border = "1px solid #FF3591";
 	} else {
-		button.style.backgroundColor = "#858585";
+		button.style.backgroundColor = "#262626";
 		button.style.color = "white";
-		console.log("Supuestamente cambiado a negro");
-	}
+		button.style.border = "1px solid white";
 
-	console.log(button.style.backgroundColor);
+	}
 
 }
